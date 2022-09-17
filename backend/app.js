e3f0e988ef6475f10ae934fe489295b0948fa4cd
Abaz <abaz.my.id@gmail.com>
2022-09-17 15:57:48 +0700

    intial backend code

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var { graphqlHTTP } = require('express-graphql');
+var { schema, root } = require('./graphql/UserSchema')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,5 +18,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  }));
 
 module.exports = app;
